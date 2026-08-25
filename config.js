8676f88363ce8d1715360d5e15945fb00e338f36
devanayak-6260 <devanayak62600@gmail.com>
2026-08-25 05:45:38 +0530

    Add files via upload

---
+++ config.js
@@ -0,0 +1,4 @@
+require('dotenv').config();
+module.exports={botName:'DEVA XMD-BOT',ownerName:'DEVA',ownerNumber:process.env.OWNER_NUMBER||'',prefix:process.env.PREFIX||'.',sessionName:'deva-session',pairingCode:true,
+autoStatusSeen:true,autoStatusLike:true,autoReact:true,alwaysOnline:true,autoTyping:true,autoRecording:true,autoCallReject:true,antiDelete:true,autoReply:true,smartAutoReply:true,autoReplyKeywords:{},
+reactEmoji:['❤️','🔥','👍','😍','😂','💯'],customReactEmoji:[],statusReaction:['❤️','🔥','👍','😍'],timezone:'Asia/Kolkata'};
