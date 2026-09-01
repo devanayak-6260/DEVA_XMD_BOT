0b86a0082d762a3dc7aea7a6b05a8f337ef3db2a
DEVA NAYAK <devanayak62600@gmail.com>
2026-09-01 11:35:27 +0530

    Delete config.js

--- config.js
+++
@@ -1,4 +0,0 @@
-require('dotenv').config();
-module.exports={botName:'DEVA XMD-BOT',ownerName:'DEVA',ownerNumber:process.env.OWNER_NUMBER||'',prefix:process.env.PREFIX||'.',sessionName:'deva-session',pairingCode:true,
-autoStatusSeen:true,autoStatusLike:true,autoReact:true,alwaysOnline:true,autoTyping:true,autoRecording:true,autoCallReject:true,antiDelete:true,
-reactEmoji:['❤️','🔥','👍','😍','😂','💯'],customReactEmoji:[],statusReaction:['❤️','🔥','👍','😍'],timezone:'Asia/Kolkata'};
