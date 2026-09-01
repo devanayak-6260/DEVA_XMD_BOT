7650fa170d173b153532b0b3522b14aa9b4927fa
DEVA NAYAK <devanayak62600@gmail.com>
2026-09-01 11:33:36 +0530

    Delete commands directory

--- commands/help.js
+++
@@ -1,1 +0,0 @@
-const menu=require('./menu'); module.exports=menu;
--- commands/owner.js
+++
@@ -1,1 +0,0 @@
-const config=require('../config'); module.exports=async(sock,msg)=>sock.sendMessage(msg.key.remoteJid,{text:`👑 *${config.ownerName||'DEVA'}*\n📱 +${String(config.ownerNumber||'').replace(/\D/g,'')||'Not set'}\n🤖 ${config.botName}`},{quoted:msg});
